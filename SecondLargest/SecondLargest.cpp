145cf00aa1871204ab43ac8254acd7152ca20838
Roshani Mathur <roshanimathur333@gmail.com>
2026-08-15 11:35:03 +0530

    Add  second largest element solution
    Added a comment block explaining the input and output for the second largest element function.

--- SecondLargest/SecondLargest.cpp
+++ SecondLargest/SecondLargest.cpp
@@ -1,3 +1,8 @@
+/*nput: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34
+*/
+
 class Solution {
   public:
     int getSecondLargest(vector<int> &a) {
